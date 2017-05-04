18b654b636aab0743d40738c2f176d9a42d89aae
K.Dicke <k.dicke7@gmail.com>
2017-05-03 18:29:27 -0700

    Controller straight up handled bitch

---
+++ controllers/carsController.js
@@ -0,0 +1,33 @@
+const carModel = require('../models/Cars.js')
+
+
+module.exports = {
+  home: function(req, res){
+    res.send('Welcome to the home page')
+  },
+  list: function(req, res){
+    carModel.find({}, (err, cars) => {
+      res.json(cars)
+    })
+  },
+  create: function(req, res){
+    carModel.create(req.body, (err, newCar) => {
+      res.json({success: true, car: newCar})
+    })
+  },
+  show: function(req,res){
+    carModel.findById(req.params.id, (err, singleCar) => {
+      res.json(singleCar)
+    })
+  },
+  edit: function(req, res){
+    carModel.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updateCar) => {
+      res.json({success:true, car: updatedCar})
+    })
+  },
+  delete: function(req, res){
+    carModel.findByIdAndRemove(req.params.id, (err, deleteCar) => {
+      res.json({success: true, car: deletedCar})
+    })
+  }
+}
