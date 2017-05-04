68aeb3b59ab866db9e68a79f6e3898d4ada1e348
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-05-03 17:11:08 -0700

    added route shells

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ const
   express = require('express'),
   app = express()
 
+ app.get('/', (req, res) => {
 
+ })
+
+ app.get('/cars', (req, res) => {
+
+ })
+
+ app.post('/cars/:id', (req, res) => {
+
+ })
+
+ app.get('/cars/:id', (req,res) => {
+
+ })
+
+ app.patch('cars/:id', (req,res) => {
+
+ })
+
+ app.delete('cars/:id', (req,res) => {
+
+ })
 
   app.listen(1234, (err) => {
     console.log(err || "Server running on 1234")
